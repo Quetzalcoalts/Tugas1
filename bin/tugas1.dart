1c0211cfc5e7d3ab2aa6d76822474e9956162b01
Quetzalcoalts <99593775+Quetzalcoalts@users.noreply.github.com>
2022-03-05 19:17:17 +0700

    Versi belum yang genap

--- bin/tugas1.dart
+++ bin/tugas1.dart
@@ -6,44 +6,46 @@ void main(List<String> arguments) {
   String? kata = stdin.readLineSync();
   int angka = int.parse(kata!);
   int c = 0;
+  int counter = 1;
 
   for (int i = 1; i <= angka; i++) {
+    if (i >= 2 && i <= (angka / 2).ceil()) {
+      c = c + 1;
+    } else if (i >= (angka / 2).ceil() + 1 && i <= angka) {
+      c = c - 1;
+    }
+    if (counter > 9) {
+      counter = 1;
+    }
+
     for (int j = 0; j < angka; j++) {
       //atas dan bawah
+
       if (i == 1 || i == angka) {
         if (j == (angka - 1) / 2) {
-          stdout.write("$i");
+          stdout.write("$counter");
         } else {
           stdout.write("*");
         }
       } //else if buat yang ditengah
       else if (i == (angka + 1) / 2) {
-        if (j == 0 || j == (angka - 1) / 2 || j == angka - 1) {
-          stdout.write("$i");
+        if (j % 2 == 0) {
+          stdout.write("$counter");
         } else {
           stdout.write(" ");
         }
       } else {
-        if (i % 2 == 0) {
-          if (j == ((angka + 1) / 2) - 2 || j == ((angka + 1) / 2)) {
-            stdout.write("$i");
-          } else if (j <= ((angka + 1) / 2) - 2 || j >= ((angka + 1) / 2)) {
-            stdout.write("*");
-          } else {
-            stdout.write(" ");
-          }
-        } //else buat yang baris 3 sama baris 5
-        else {
-          if (j == ((angka + 1) / 2) - 3 || j == (angka + 1) / 2 + 1) {
-            stdout.write("$i");
-          } else if (j <= ((angka + 1) / 2) - 3 || j >= ((angka + 1) / 2) + 1) {
-            stdout.write("*");
-          } else {
-            stdout.write(" ");
-          }
+        if (j == ((angka - 1) / 2) - c || j == ((angka - 1) / 2) + c) {
+          stdout.write("$counter");
+        } else if (j >= ((angka - 1) / 2) - c && j <= (angka - 1) / 2 ||
+            j <= ((angka - 1) / 2) + c && j >= (angka - 1) / 2) {
+          stdout.write(" ");
+        } else {
+          stdout.write("*");
         }
       }
     }
     print("");
+    counter++;
   }
 }
